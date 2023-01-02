ce63f8032bfe215832ac1546cf75a69f8b571e8f
SunilSubedi <sunilsubedi9@gmail.com>
2023-01-02 16:18:29 +1100

    Added Bootstrap
    added bootstrap file and using class

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 //import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap';
 
 function App() {
   return (
-   <div>
-
+   <div className="container">
+        <div className="row">
+          <div className="col-4">Column</div>
+          <div className="col-8">Column</div>
+        </div>
    </div>
   );
 }
